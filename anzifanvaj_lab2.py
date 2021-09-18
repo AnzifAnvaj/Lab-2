14c2b29c812ed618dced7d30b83670d9320092d6
Anzif Anvaj <90127070+AnzifAnvaj@users.noreply.github.com>
2021-09-18 13:20:11 +0400

    Add files via upload

---
+++ anzifanvaj_lab2.py
@@ -0,0 +1,19 @@
+import turtle
+
+def test_drive():
+    turtle.forward(100)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.goto(50,50)
+    turtle.home()
+    turtle.circle(25)
+    turtle.circle(25)
+
+def main_turtle():
+    test_drive()
+    input("Press any key to continue.....")
+
+main_turtle()
+
+
+    
